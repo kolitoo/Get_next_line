fa533f372429eaec83599a03802861642f37e096
Alexandre Bourdon <99012539+kolitoo@users.noreply.github.com>
2022-12-05 16:46:34 +0000

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,158 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abourdon <abourdon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 14:50:47 by abourdon          #+#    #+#             */
+/*   Updated: 2022/12/05 13:26:49 by abourdon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*If 0, check if str[i] == c and return his index
+If 1, check the number of '-1' in the string and return his index*/
+int	check_cara(char *str, char c, int bool)
+{
+	int	i;
+
+	i = 0;
+	if (bool == 0)
+	{
+		while (str[i] != c && str[i] != '\0')
+			i++;
+		if (str[i] == c)
+			return (i + 1);
+		return (0);
+	}
+	else
+	{
+		while (str[i] == -1)
+			i++;
+		return (i);
+	}
+}
+
+/*If 0, init the string with c.
+If 1, init the string with '\0'*/
+void	init_str(char *buff, char c, int bool)
+{
+	int	i;
+
+	i = -1;
+	if (bool == 0)
+	{
+		while (buff[++i] != '\n')
+			buff[i] = c;
+		buff[i] = c;
+	}
+	else
+	{
+		while (++i <= BUFFER_SIZE)
+			buff[i] = c;
+	}
+}
+
+/*create line with join. "clean" buff with '\0' for the 
+next call and read again*/
+int	no_new_line(char *buff, char **line, int fd, int *red)
+{
+	*line = join(*line, substr(buff, check_cara(buff, -1, 1),
+				check_cara(buff, '\0', 0) - check_cara(buff, -1, 1)));
+	init_str(buff, '\0', 1);
+	*red = read(fd, buff, BUFFER_SIZE);
+	if (*red < 0)
+	{
+		free (*line);
+		return (0);
+	}
+	return (1);
+}
+
+/*Init red = 1. If -1 => read*/
+int	init_read(char *buff, int *red, int fd, char **line)
+{
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	*line = ft_calloc(1, sizeof(char));
+	if (!*line)
+		return (0);
+	*red = 1;
+	if (buff[0] != -1)
+	{
+		*red = read(fd, buff, BUFFER_SIZE);
+		if (*red < 0)
+		{
+			free (*line);
+			return (0);
+		}
+	}
+	return (1);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	char		*line;
+	int			red;
+
+	buff[BUFFER_SIZE] = '\0';
+	if (init_read(buff, &red, fd, &line) == 0)
+		return (NULL);
+	while (red != 0)
+	{
+		if (check_cara(buff, '\n', 0) != 0)
+		{
+			line = join(line, substr(buff, check_cara(buff, -1, 1),
+						check_cara(buff, '\n', 0) - check_cara(buff, -1, 1)));
+			init_str(buff, -1, 0);
+			return (line);
+		}
+		if (no_new_line(buff, &line, fd, &red) == 0)
+			return (NULL);
+	}
+	if (free_line(line) == 1)
+		return (NULL);
+	return (line);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+// Pour lire plusieurs lignes merci de specifier ce nombre en unique argument de l'executable
+// Pour lire plusieurs lignes dans un ficher merci de specifier le nombre en 1er argument et le file en second (relative or absolute path)
+
+
+int	main(int argc, char **argv)
+{
+	int		line_to_read;
+	int		fd;
+	char	*file_path;
+	char	*line;
+
+	line_to_read = 1;
+	file_path = "test.txt";
+	if (argc >= 2)
+		line_to_read = atoi(argv[1]);
+	if (argc == 3)
+		file_path = argv[2];
+	else
+		(void) argv;
+	fd = open(file_path, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("fd < 0\nExiting program ...");
+		return (-1);
+	}
+	else
+		printf("fd :%i\n", fd);
+	while (line_to_read--)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		free(line);
+	}
+}
